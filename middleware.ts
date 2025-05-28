060d0b7ac6f1e2b9df937a86d52e2f3660a36579
simonbjerkas <simon.bjerkas10@gmail.com>
2025-05-28 21:11:51 +0200

    middleware update

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,14 @@ import {
 } from '@convex-dev/auth/nextjs/server';
 
 const isSignInPage = createRouteMatcher(['/signin']);
+const isProtectedRoute = createRouteMatcher(['/watchlist', '/entries']);
 
 export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
   if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
     return nextjsMiddlewareRedirect(request, '/');
+  }
+  if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
+    return nextjsMiddlewareRedirect(request, '/signin');
   }
 });
 
